20893048decfbe6709191e8973c7c0f6ccbd74a1
Peter Tomaselli <ptomaselli@ngpvan.com>
2016-07-08 10:04:06 -0400

    Uninfer: first commit!

---
+++ Uninfer/Uninfer.Test/UnitTests.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using Uninfer;
+
+namespace Uninfer.Test
+{
+	[TestClass]
+	public class UnitTest : CodeFixVerifier
+	{
+		//No diagnostics expected to show up
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var test = @"";
+
+			VerifyCSharpDiagnostic(test);
+		}
+
+		[TestMethod]
+		public void TestSomething()
+		{
+			string test = @"
+using System;
+using System.Text;
+
+namespace Foo
+{
+	class Bar
+	{
+		void Baz()
+		{
+			var qux = string.Empty;
+		}
+	}
+}
+";
+			DiagnosticDescriptorData varIsBad = new VarIsBad();
+			DiagnosticResult expected = new DiagnosticResult
+			{
+				Id = varIsBad.Id,
+				Message = string.Format(varIsBad.Format, "`var` is bad."),
+				Severity = DiagnosticSeverity.Info,
+				Locations = new[] { new DiagnosticResultLocation("Test0.cs", 11, 4) }
+			};
+			VerifyCSharpDiagnostic(test, expected);
+		}
+
+		[TestMethod]
+		public void TestSomethingElse()
+		{
+			string test = @"
+using System;
+using System.Text;
+
+namespace Foo
+{
+	class Bar
+	{
+		void Baz()
+		{
+			string theM = ""M"";
+		}
+	}
+}
+";
+			DiagnosticDescriptorData doNotNameVariablesTheM = new DoNotNameVariablesTheM();
+			DiagnosticResult expected = new DiagnosticResult
+			{
+				Id = doNotNameVariablesTheM.Id,
+				Message = string.Format(doNotNameVariablesTheM.Format, "Do not name variables `theM`."),
+				Severity = DiagnosticSeverity.Warning,
+				Locations = new[] { new DiagnosticResultLocation("Test0.cs", 11, 4) }
+			};
+			VerifyCSharpDiagnostic(test, expected);
+		}
+
+	//	//Diagnostic and CodeFix both triggered and checked for
+	//	[TestMethod]
+	//	public void TestMethod2()
+	//	{
+	//		var test = @"
+	//using System;
+	//using System.Collections.Generic;
+	//using System.Linq;
+	//using System.Text;
+	//using System.Threading.Tasks;
+	//using System.Diagnostics;
+
+	//namespace ConsoleApplication1
+	//{
+	//	class TypeName
+	//	{
+	//	}
+	//}";
+	//		var expected = new DiagnosticResult
+	//		{
+	//			Id = "Uninfer",
+	//			Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
+	//			Severity = DiagnosticSeverity.Warning,
+	//			Locations =
+	//				new[] {
+	//						new DiagnosticResultLocation("Test0.cs", 11, 15)
+	//					}
+	//		};
+
+	//		VerifyCSharpDiagnostic(test, expected);
+
+	//		var fixtest = @"
+	//using System;
+	//using System.Collections.Generic;
+	//using System.Linq;
+	//using System.Text;
+	//using System.Threading.Tasks;
+	//using System.Diagnostics;
+
+	//namespace ConsoleApplication1
+	//{
+	//	class TYPENAME
+	//	{
+	//	}
+	//}";
+	//		VerifyCSharpFix(test, fixtest);
+	//	}
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider()
+		{
+			return new UninferCodeFixProvider();
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+		{
+			return new UninferAnalyzer();
+		}
+	}
+}
---
+++ Uninfer/Uninfer/Analyzer/UninferAnalyzer.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Uninfer
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class UninferAnalyzer : DiagnosticAnalyzer
+	{
+		#region DiagnosticAnalyzer
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+		{
+			get
+			{
+				return ImmutableArray.Create(DoNotNameVariablesTheM.BuiltDescriptor(), VarIsBad.BuiltDescriptor());
+			}
+		}
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.VariableDeclaration);
+		}
+
+		#endregion
+
+		private readonly DiagnosticDescriptorData DoNotNameVariablesTheM = new DoNotNameVariablesTheM();
+
+		private readonly DiagnosticDescriptorData VarIsBad = new VarIsBad();
+
+		private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+		{
+			VariableDeclarationSyntax myNode = context.Node as VariableDeclarationSyntax;
+			if (myNode == null)
+				return;
+
+			if (myNode.Type.IsVar)
+			{
+				// exterminate
+				Diagnostic diagnostic = Diagnostic.Create(VarIsBad.BuiltDescriptor(), myNode.GetLocation(), "`var` is bad.");
+				context.ReportDiagnostic(diagnostic);
+			}
+
+			if (myNode.Variables.Any(v => v.Identifier.Text == "theM"))
+			{
+				Diagnostic diagnostic = Diagnostic.Create(DoNotNameVariablesTheM.BuiltDescriptor(), myNode.GetLocation(), "Do not name variables `theM`.");
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		private void Junk()
+		{
+			var foo = "foo";
+		}
+	}
+
+	interface DiagnosticDescriptorData
+	{
+		string Category { get; }
+
+		DiagnosticSeverity DefaultSeverity { get; }
+
+		string Format { get; }
+
+		string Id { get; }
+
+		bool IsEnabledByDefault { get; }
+
+		string Title { get; }
+	}
+
+	static class DiagnosticDescriptorDataExtensions
+	{
+		internal static DiagnosticDescriptor BuiltDescriptor(this DiagnosticDescriptorData descriptorData)
+		{
+			return new DiagnosticDescriptor(descriptorData.Id, descriptorData.Title, descriptorData.Format, descriptorData.Category, descriptorData.DefaultSeverity, descriptorData.IsEnabledByDefault);
+		}
+	}
+
+	struct DoNotNameVariablesTheM : DiagnosticDescriptorData
+	{
+		public string Category { get { return ""; } }
+
+		public DiagnosticSeverity DefaultSeverity { get { return DiagnosticSeverity.Warning; } }
+
+		public string Format { get { return "Hey! {0}"; } }
+
+		public string Id { get { return "UIF002"; } }
+
+		public bool IsEnabledByDefault { get { return true; } }
+
+		public string Title { get { return "theM"; } }
+	}
+
+	struct VarIsBad : DiagnosticDescriptorData
+	{
+		public string Category { get { return ""; } }
+
+		public DiagnosticSeverity DefaultSeverity { get { return DiagnosticSeverity.Info; } }
+
+		public string Format { get { return "Hey! {0}"; } }
+
+		public string Id { get { return "UIF001"; } }
+
+		public bool IsEnabledByDefault { get { return true; } }
+
+		public string Title { get { return "Uninferred"; } }
+	}
+}
